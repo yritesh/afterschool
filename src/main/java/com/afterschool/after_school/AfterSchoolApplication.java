f5c7bbd1122f3301a7ec0362076f04397428d4bf
Ritesh <yritesh7@gmail.com>
2024-06-23 15:35:37 +0530

    first commit

---
+++ src/main/java/com/afterschool/after_school/AfterSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.afterschool.after_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afterschool/after_school/AfterSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afterschool.after_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
